e43a86e07a6d83755d30570913513fe1392a598d
Joseph Fiddes <fiddj13@live.com.au>
2025-05-20 16:11:13 +1000

    Added maintenance notice

--- include/header.php
+++ include/header.php
@@ -133,6 +133,10 @@ TODO:
 				<table><tr><td>CONTACT</td></tr></table>
 			</a>
 		</div>
+
+		<div>
+			<h1>UNDER MAINTENANCE - PLEASE BE PATIENT!!!</h1>
+		</div>
 	</div>
 
 	<script src="./scripts/header_social_media_buttons.js"></script>
