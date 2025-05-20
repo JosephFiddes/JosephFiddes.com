6f0052a0b5ef87b8397fb229f804396e10bab3a7
Joseph Fiddes <fiddj13@live.com.au>
2025-05-20 18:29:06 +1000

    logs part of pwd for debugging purposes

--- include/dbh.inc.php
+++ include/dbh.inc.php
@@ -1,9 +1,13 @@
 <?php
 
+include_once "include/helper.php";
+
 $dbServername = getenv("DB_HOST");
 $dbUsername = getenv("DB_USERNAME");
 $dbPassword = getenv("DB_PASSWORD");
 $dbName = getenv("DB_DATABASE");
+
+console_log(substr($dbPassword, 0, 10));
 
 $conn = mysqli_connect($dbServername, $dbUsername, 
 	$dbPassword, $dbName);
