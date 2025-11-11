a9ec16f01c87b9cc13c08c3a8b5c711014245033
Joseph Fiddes <fiddj13@live.com.au>
2025-11-11 11:01:05 +1100

    Remove contact form due to third-party service outage

--- contact.php
+++ contact.php
@@ -1,44 +1,16 @@
 <?php
 	include_once 'include/header.php';
 ?>
-	<div class="page-description">
-		Please use this form to contact me for inquiries you may have. Please fill out all fields so I can reply.
-	</div>
 
-	<div class="form">
-		<form action="https://formsubmit.co/2c34a560b5f9ab812e80989b1772ba1b" method="POST">
-			<!-- Name -->
-			<div class="form-field">
-				<label for="name">Name:</label>
-				<input class="textbox small-textbox" type="text" id="name" name="name"><br>
-			</div>
+	<h1> Apologies: </h1>
 
-			<!-- Email -->
-			<div class="form-field">
-				<label for="email">Email:</label>
-				<input class="textbox small-textbox" type="email" id="email" name="email" required><br>
-			</div>
+	<h2> This page used to contain a contact form which would send correspondance to my email. At the time of writing, the third-party service which sent the emails is down. At this time, I'm unsure if this is temporary or permament, but in the interest of security, I've removed the contact form from this page. </h2>
 
-			<!-- Subject -->
-			<div class="form-field">
-				<label for="subject">Subject:</label>
-				<input class="textbox small-textbox" type="text" id="subject" name="subject"><br>
-			</div>
+	<p> If you want to contact me, please download a copy of my CV (on the bottom of the 'CV' page) and use the email on the header of the document. </p>
 
-			<!-- Message -->
-			<div class="form-field">
-				<label for="message">Message:</label><br>
-				<textarea class="textbox large-textbox" rows="20" cols="60" name="message" required></textarea><br>
-			</div>
+	<p> I apologise again for the inconvenience. I will soon put a more permanent solution here. </p>
+	
 
-			<!-- Submit -->
-			<div class="centre-div" style="text-align: center;">
-				<input class="button" type="submit" id="submit" name="submit" value="SUBMIT"><br>
-			</div>
-
-			<input type="hidden" name="_next" value="https://josephfiddes.com/">
-		</form>
-	</div>
 	<?php
 		include_once 'include/footer.php';
 	?>
